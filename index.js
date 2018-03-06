46da7d87b1e811d79604a850562b70f21ac57a92
just-mendelson <jmendels@usc.edu>
2018-03-06 01:41:26 -0800

    More added for heatmaps

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import { scroll, scrollTop } from "./ui/Utilities.js";
+import Heat from "./ui/Heat.js";
 
 const ELEMENTS = { // Here is the constant elements for the site
     nav: document.body.querySelector("nav"),
     navLinks: Array.from(document.body.querySelectorAll("header > nav > a")),
     sections: new Map(Array.from(document.body.querySelectorAll("section")).map(section => ["#" + section.id, section])),
+
+    heats: Array.from(document.body.querySelectorAll("#Comparison .heat")),
 };
 
 for (let link of ELEMENTS.navLinks) { // Going to the top of each section using the hyperlinks in the menu
@@ -24,3 +27,50 @@ function handleScroll(event) {
 }
 window.addEventListener("scroll", handleScroll);
 handleScroll();
+
+d3.queue()
+    .defer(d3.csv, "data/2014-thermalPP-data.csv")
+    .await((error, csv2014) => {
+        let formattedHeat = {};
+        let states = new Set;
+        function parseCSV(csv, year) {
+            formattedHeat[year] = Object.values(csv.reduce((accumulator, item) => {
+                let state = item["State.ID"];
+                states.add(state);
+                if (!(state in accumulator))
+                    accumulator[state] = {};
+
+                let key = item["Latitude"] + item["Longitude"];
+                if (!(key in accumulator)) {
+                    let x = parseFloat(item["Latitude"]);
+                    let y = parseFloat(item["Longitude"]);
+                    accumulator[state][key] = {
+                        x,
+                        y,
+                        data: new Heat.Point(x, y),
+                    };
+                }
+                accumulator[state][key].data.add(item["Consolidated.Description"]);
+                return accumulator;
+            }, {}));
+            for (let key in formattedHeat[year])
+                formattedHeat[year][key] = Object.values(formattedHeat[year][key]);
+        }
+        parseCSV(csv2014, 2014);
+
+        function createHeat(container, {state, year}) {
+            let heat = new Heat(formattedHeat, {
+                container,
+                zoom: 12,
+                defaultState: state,
+                defaultYear: year,
+            });
+            heat.element.style.setProperty("height", heat.element.offsetWidth + "px");
+        }
+
+        let area = pick(Array.from(areas));
+        createHeat(ELEMENTS.heats[0], {
+            state,
+            year: "2014",
+        });
+    });
